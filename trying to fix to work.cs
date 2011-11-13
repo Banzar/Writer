5a3f687f13b652b960cf865ab9dea978675cb9ca
David Ledford <dcledford@ymail.com>
2011-11-13 18:02:23 -0500

    lowered .net framework

---
+++ trying to fix to work.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: David Ledford
+ Date: 11/6/2011
+ * Time: 6:39 PM
+ */
+using System;
+using System.IO;
+using System.Security.AccessControl;
+using FileSystemExample;
+Imports System.Runtime.InteropServices
+
+namespace FileSystemExample
+{
+    class FileExample
+    {
+        public static void Main()
+        {
+            try
+            {
+                string FileName = "test.xml";
+
+                Console.WriteLine("Encrypt " + FileName);
+
+                // Encrypt the file.
+                AddEncryption(FileName);
+
+                Console.WriteLine("Decrypt " + FileName);
+
+                // Decrypt the file.
+                RemoveEncryption(FileName);
+
+                Console.WriteLine("Done");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadLine();
+        }
+
+
+        // Encrypt a file.
+        public static void AddEncryption(string FileName)
+        {
+
+            File.Encrypt(FileName);
+
+        }
+
+        // Decrypt a file.
+        public static void RemoveEncryption(string FileName)
+        {
+            File.Decrypt(FileName);
+        }
+    }
+}
+
